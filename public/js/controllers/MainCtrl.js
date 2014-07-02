f804431262343e34fa52f62557bc303d0e60158c
Christoph Krieger <christoph-krieger1@gmx.de>
2014-07-03 00:02:46 +0200

    added css

--- public/js/controllers/MainCtrl.js
+++ public/js/controllers/MainCtrl.js
@@ -1,17 +1,29 @@
 angular.module('MainCtrl', []).controller('MainController', function($scope, $http, $location) {
-	 // gets the top 5 sentences
-   $http({method: 'POST', url: '/getRandomSentence'}).success(function (data){
-   	$scope.sentence = data;
-   	var totalVotes =$scope.sentence.senseVote +$scope.sentence.noSenseVote +$scope.sentence.notSureVote;
+	 
+ 
+
+   $scope.goToCreateSentence = function(){
+    $location.path('/createSentence');
+  };
+
+   $scope.goToTopFive = function(){
+    $location.path('/topFive');
+  };
+
+  $scope.refresh = function(){
+     $http({method: 'POST', url: '/getRandomSentence'}).success(function (data){
+    $scope.sentence = data;
+    var totalVotes =$scope.sentence.senseVote +$scope.sentence.noSenseVote +$scope.sentence.notSureVote;
     if (totalVotes != 0){
-    	$scope.sentence.percentage = (100/totalVotes) *$scope.sentence.noSenseVote;}
+      $scope.sentence.percentage = (100/totalVotes) *$scope.sentence.noSenseVote;}
      else {
         $scope.sentence.percentage = 0;
      }
    });
+  }
 
-   $scope.goToCreateSentence = function(){
-    $location.path('/createSentence');
+  $scope.goToHome = function(){
+    $location.path('/')
   }
 
 });
