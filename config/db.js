7fac791e5b112c0f66b17a9b6749aeb9a448a6b4
Christoph Krieger <christoph-krieger1@gmx.de>
2014-08-12 13:16:28 +0200

    no message

--- config/db.js
+++ config/db.js
@@ -1,3 +1,3 @@
 module.exports = {
-	url : 'mongodb://chris:8452b699e3a09deca48f4d5f680a3428@kahana.mongohq.com:10042/app28165760'
+	url : 'mongodb://chris:noSentenceSense@kahana.mongohq.com:10042/app28165760'
 }
