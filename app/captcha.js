bdc1e31da7d86b1cda15c8928661996619ff2fea
Christoph Krieger <christoph-krieger1@gmx.de>
2014-10-16 18:00:29 +0200

    updated mailer.js  mail contains now id of reported sentence

--- app/captcha.js
+++ app/captcha.js
@@ -4,8 +4,8 @@ var createCaptcha = function(req , res){
 	var captcha = parseInt(Math.random()*9000+1000); 
 	req.session.captcha = captcha;
 	var p = captchaGen(80,30, captcha);
-       p.color(0, 0, 0, 0);  // First color: background (red, green, blue, alpha)
-        p.color(80, 80, 80, 255); // Second color: paint (red, green, blue, alpha)
+       p.color(0, 0, 0, 0);  
+        p.color(80, 80, 80, 255); 
 
         var img = p.getBase64();
         var imgbase64 = new Buffer(img,'base64');
--- app/mailer.js
+++ app/mailer.js
@@ -1,7 +1,7 @@
 var nodemailer = require("nodemailer");
 var config = require('./models/Config.js');
 
-var sendMail = function(mail, mailPassword, mailAddressee, selectedReason) {
+var sendMail = function(mail, mailPassword, mailAddressee, selectedReason, sentenceId) {
     var smtpTransport = nodemailer.createTransport("SMTP", {
         service: "Gmail",
         auth: {
@@ -10,11 +10,11 @@ var sendMail = function(mail, mailPassword, mailAddressee, selectedReason) {
         }
     });
     var mailOptions = {
-            from: "Fred Foo ✔ <foo@blurdybloop.com>", // sender address
+            from: "NoSentenceSense <nosentencesensepage@gmail.com>", // sender address
             to: mailAddressee, // list of receivers
-            subject: "Satz gemeldet Reason  " + selectedReason, // Subject line
-            text: "hi was geht", // plaintext body
-            html: "<b>Hello world ✔</b>" // html body
+            subject: "Satz gemeldet Grund:  " + selectedReason.item , // Subject line
+            text: "Der Satz mit der ID:" , // plaintext body
+            html: "<b>Der gemeldete Satz hat folgende Id : </b>" + sentenceId + "</br> <b>es liegt folgende Beschwerde vor : </b>" + selectedReason.item // html body
         }
         // send mail with defined transport object
     smtpTransport.sendMail(mailOptions, function(error, response) {
